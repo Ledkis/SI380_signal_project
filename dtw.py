b4cd9f7859cc2b639afb30a45b10814d2fd591ec
Ledkis <emile.quenee@gmail.com>
2014-05-21 21:13:49 +0200

    Imlementation of the evaluation of cummulated distance matrix function

--- dtw.py
+++ dtw.py
@@ -11,7 +11,7 @@ def eucl_dist(x, y):
 
 
 def eval_distance_matrix(s1, s2,  d_eval = lambda x,y : (x - y)**2):
-    """Create the distance matrix between the time series s1 and s2 using
+    """Evaluate the distance matrix between the time series s1 and s2 using
     the distance d_eval.
     
     Parameter
@@ -22,23 +22,61 @@ def eval_distance_matrix(s1, s2,  d_eval = lambda x,y : (x - y)**2):
     
     """
     
-    mm = s1.size
-    nn = s2.size
+    nn = s1.size
+    mm = s2.size
     
     #Initialisation of the distance matrix
-    D = np.zeros((mm, nn));
+    D = np.zeros((nn, mm));
     
-    for i in range(mm):
-        for j in range(nn):
+    for i in range(nn):
+        for j in range(mm):
             D[i, j] = d_eval(s1[i], s2[j])
             
     return D
+    
+def eval_cummuled_distance_matrix(D, r = None, warping_window = lambda i,j,r : i-r<=j<=i+r):
+    """Evaluate the cummuled distance matrix W from a distance matrix D
+    
+    Parameter
+    D -- distance matrix
+    r -- wraping windows
+    """
+    
+    #Use warping or not
+    warp = False
+    if r is not None:
+        warp = True
+    
+    nn, mm = np.shape(D)
+    W = np.zeros((nn, mm))
+    
+    #REM : We will use there a Symetric DTW wit warping windows and no slope constraint
+    
+    for i in range(nn):
+        for j in range(mm):
+            #Initialisation
+            if i == j == 0:
+                W[i, j] = D[0, 0]
+            else:
+                if not (warp and warping_window(i, j, r)):
+                    continue;
+                d = []
+                if i > 0:
+                    d.append(W[i-1, j] + D[i, j])
+                if i > 0 and j > 0:
+                    d.append(W[i-1, j-1] + 2*D[i, j])
+                if j > 0:
+                    d.append(W[i, j-1] + D[i,j])
+                W[i,j] = min(d)
+    return W
+    
 	
-def display_distance_matrix(D):
+def display_row_inverted_matrix(D):
     """Display the distance matrix with the origin in the bottom left corner.
 	"""
     D = np.flipud(D)
     print(D)
+
     
     
 	
@@ -52,5 +90,11 @@ if __name__ == "__main__":
     D2= eval_distance_matrix(s3, s1)
     
     
-    display_distance_matrix(D1)
-    display_distance_matrix(D2)
+    display_row_inverted_matrix(D1)
+    display_row_inverted_matrix(D2)
+    
+    W1 = eval_cummuled_distance_matrix(D1, 4)
+    W2= eval_cummuled_distance_matrix(D2, 4)
+    
+    display_row_inverted_matrix(W1)
+    display_row_inverted_matrix(W2)
