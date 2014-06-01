b7fb8cafc307ec6eb53a6453703e901eccbcd4ad
Ledkis <emile.quenee@gmail.com>
2014-06-02 00:32:27 +0200

    Add module for monitoring accelerometer signal using pygame

--- user_manager.py
+++ user_manager.py
@@ -6,7 +6,10 @@ Created on Tue May 27 15:40:24 2014
 """
 
 import m_log as Log
+import input_data_display
 
+import sys
+import traceback
 import threading
 
 
@@ -19,13 +22,15 @@ class User_manager():
         
         self.user_list = []
         
+        self.monitor = input_data_display.Signal_Monitor(500, 900)
+        
         self.debug = debug
         self.TAG = "User_manager"
         Log.d(self.TAG, "Initialized", self.debug)
         
         
     def new_user(self, name, connexion):
-        self.user_list.append(User(name, connexion, self.gesture_database_manager))
+        self.user_list.append(User(name, connexion, self))
         
     def start_user(self, user_name):
         for user in self.user_list:
@@ -35,20 +40,24 @@ class User_manager():
     def start_all_users(self):
         for user in self.user_list:
             user.start()
+            
+    def stop_all_users(self):
+        for user in self.user_list:
+            user.stop()
         
     def stop_user(self, user_name):
         for user in self.user_list:
             if user.name == user_name:
                 user.start()
-                
 
 
 class User(threading.Thread):
-    def __init__(self, name, connexion, gesture_database_manager, debug = True):
+    def __init__(self, name, connexion, user_manager, debug = True):
         threading.Thread.__init__(self) 
+        self.manager = user_manager
         self.name = str(name)
         self.connexion = connexion
-        self.gesture_database_manager = gesture_database_manager
+        self.gesture_database_manager = user_manager.gesture_database_manager
         
         self.app_mode = False
         
@@ -64,32 +73,57 @@ class User(threading.Thread):
         self.gesture = []
         self.gesture_list = []
         
+        self.values  = [0, 0, 0]
+        
+        self.manager.monitor.init_data_ref(self.values)
+        
+        self.manager.monitor.start()
+        
         while not self.done:
-            data = self.connexion.receive()
-            self._data_treatment(data)
+            try:
+                data = self.connexion.receive()
+            except:
+                Log.d(self.TAG, "[ERROR] While receiving : %s"%(sys.exc_info()[0]), self.debug)
+                traceback.print_exc()
+            try:
+                self._data_treatment(data)
+            except:
+                Log.d(self.TAG, "[ERROR] While _data_treatment : %s"%(sys.exc_info()[0]), self.debug)
+                traceback.print_exc()
+               
+        Log.d(self.TAG, "End run", self.debug)
+               
             
                 
     def _data_treatment(self, in_data):
         
         data_list = in_data.split("\n")
         
-        
-        
         for data in data_list:
             data = data.split(";")
             msg = data[0]
             
             if msg == "gesture" or msg == "app":
-                x, y, z = self._acc_treatment(data[1:])
-                self.gesture.append((x, y, z))
-                self._display_acc(x, y, z, "[%s]"%msg)
+                try:
+                    if len(data[1:]) == 3: #TODO : Can be better (by improving the messages)
+                        self.x, self.y, self.z = self._acc_treatment(data[1:])
+                        self.values[0] = self.x
+                        self.values[1] = self.y
+                        self.values[2] = self.z
+                    else :
+                        Log.d(self.TAG, "[ERROR] data not complete : %s"%(data), self.debug)
+                except:
+                    Log.d(self.TAG, "[ERROR] While _acc_treatment : %s"%(sys.exc_info()[0]), self.debug)
+                    traceback.print_exc()
+                    return
+                self.gesture.append((self.x, self.y, self.z))
+                self._display_acc(self.x, self.y, self.z, "[%s]"%msg)
                 
             if msg == "start_app":
                self.app_mode = True
                Log.d(self.TAG, "Start apprentissage", self.debug)
             
             if msg == "end_app":
-                
                 
                 gesture_name = data[1]
                 
@@ -114,6 +148,7 @@ class User(threading.Thread):
         x, y, z = data
         x, y, z, = float(x), float(y), float(z)
         return (x, y, z)
+            
     
     def _display_acc(self, x, y, z, mode = ""):
         Log.d(self.TAG, "%s%s %s %s"%(mode.ljust(SPACE), str(x).ljust(SPACE), 
@@ -124,7 +159,7 @@ class User(threading.Thread):
         
     def stop(self):
         self.done = True
-        self._stopevent.set( )
+        Log.d(self.TAG, "Stop", self.debug)
                 
     
         
